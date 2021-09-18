819508b749e7855783545fff9b7d686c9b8ed36c
hyeonji0409 <guswl8507@naver.com>
2021-09-19 00:34:33 +0900

    hello-api

--- src/main/java/com/spring_study/week1/controller/HelloController.java
+++ src/main/java/com/spring_study/week1/controller/HelloController.java
@@ -3,13 +3,20 @@ package com.spring_study.week1.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data","hello!!");
+        model.addAttribute("data", "hello!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMVC(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
